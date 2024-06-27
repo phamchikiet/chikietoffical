05dcaeceb553ffb3600900322bd581ff335cbb2d
chikiet88 <chikiet88@gmail.com>
2024-06-27 09:49:34 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { HoadonchitietModule } from './hoadonchitiet/hoadonchitiet.module';
 @Module({
   imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: '103.221.221.14',
+    //   port: 3306,
+    //   username: 'jtnkwfpz_chikiet88',
+    //   password: '@Hikiet1988',
+    //   database: 'jtnkwfpz_chikiet',
+    //   autoLoadEntities: true,
+    //   synchronize: true,
+    //   charset: "utf8mb4",
+    // }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '103.221.221.14',
-      port: 3306,
-      username: 'jtnkwfpz_chikiet88',
-      password: '@Hikiet1988',
-      database: 'jtnkwfpz_chikiet',
+      type: 'mongodb',
+      url: 'mongodb+srv://admin:@Hikiet1988@cluster0.mkgmoln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
       autoLoadEntities: true,
       synchronize: true,
-      charset: "utf8mb4",
     }),
     UsersModule,
     HoadonchitietModule
